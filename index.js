317a88dc1409b64fb99345b83b45d8bcc0811889
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-06-08 17:11:11 -0400

    news functions added

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(arrNames, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < arrNames.length; i++) {
+      let message = `Thank you, ${arrNames[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const arrNames = ["Charlie", "Samip", "Ali"];
+  const event = "birthday";
+  const messages = writeCards(arrNames, event);
+  
+  console.log(messages);
+
+  const nummm = 5;
+
+  function countDown(nummm){
+    
+      for (let i = nummm; i >= 0; i--) {
+        console.log(i)
+    }
+
+    }
